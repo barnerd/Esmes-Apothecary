cff18c3f4cd64e5408d3fa3a8241db475cbd0666
barnerd <dibarner@gmail.com>
2024-06-10 16:42:50 -0700

    Hourglass

---
+++ Objects/Hourglass.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+signal hourglass_timer_finished
+
+@onready var sand_drip = $Background/Drip
+@onready var lower_sand = $Background/BottomMask/BottomSand # should go from 487 to 167
+@onready var upper_sand = $Background/TopMask/TopSand # should go from -145 to 345
+
+
+func _ready() -> void:
+	await get_tree().create_timer(0.5).timeout
+	flip_hourglass(30)
+
+
+func flip_hourglass(drain_time: float) -> void:
+	upper_sand.position.y = -229
+	lower_sand.position.y = 513
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "rotation", 0, 1.0).from(-3.1415)
+	
+	await tween.finished
+
+	drain_sand(drain_time)
+
+
+func drain_sand(drain_time: float) -> void:
+	sand_drip.visible = true
+	var tween = get_tree().create_tween().set_parallel(true)
+	tween.tween_property(upper_sand, "position:y", 345, drain_time).from(-145)
+	tween.tween_property(lower_sand, "position:y", 167, drain_time).from(487)
+	
+	await tween.finished
+	sand_drip.visible = false
+	
+	await get_tree().create_timer(0.5).timeout
+	hourglass_timer_finished.emit()
+	reset_hourglass()
+
+func reset_hourglass() -> void:
+	#upper_sand.position.y = -229
+	#lower_sand.position.y = 513
+	#self.rotation = -3.1415
+	
+	flip_hourglass(30)
